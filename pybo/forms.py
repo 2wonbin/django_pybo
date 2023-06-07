7b3422c77b8becf0cabd532e9d22effeed7f6245
이원빈 <2wonbin@kakao.com>
2023-06-07 10:11:42 +0900

    first commit

---
+++ pybo/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from pybo.models import Question, Answer
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ["subject", "content"]
+        widgets = {
+            "subject": forms.TextInput(attrs={"class": "red"}),
+            "content": forms.Textarea(attrs={"class": "green"}),
+        }
+        labels = {
+            "subject": "제목",
+            "content": "내용",
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ["content"]
+        labels = {
+            "content": "답변내용",
+        }
